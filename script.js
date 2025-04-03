ef58625f50b70c62ab18361073d9b601d8730d6f
Gutolis <167938874+Gutolis@users.noreply.github.com>
2025-04-03 19:20:44 -0300

    A

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-
+function calculate() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    
+    if (isNaN(prev) || isNaN(current)) return;
+    
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    
+    currentOperand = computation.toString();
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
